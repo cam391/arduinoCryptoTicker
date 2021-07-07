434e078eb0a827a288678b23038cf699352a8bda
cam391 <cam391@live.unc.edu>
2021-07-06 20:29:03 -0400

    Create stockTickerDisplay.ino
    Initial commit. Code for arduino to receive data from the serial port and then display it on the lcd.

---
+++ stockTickerDisplay.ino
@@ -0,0 +1,54 @@
+// Include the lcd library:
+#include <LiquidCrystal.h>
+
+// Create an LCD object. Parameters: (RS, E, D4, D5, D6, D7):
+LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
+
+// String array of data to be displated on the lcd.
+String displayData[2];
+
+void setup() {
+  // Specify the LCD's number of columns and rows.
+  lcd.begin(16, 2);
+
+  // Set up serial w/ computer.
+  Serial.begin(115200);
+
+}
+
+void loop() {
+
+  // If the computer has sent something over the serial port.
+  if (Serial.available() > 0) {
+
+    // Wait 5 seconds
+    delay(5000);
+
+    // Read data from serial port into displayData array.
+    for (int i = 0; i < 2; i++) {
+      displayData[i] = Serial.readStringUntil(',');
+    }
+
+    // Clear display.
+    lcd.clear();
+
+    // Set cursor to first line.
+    lcd.setCursor(0, 0);
+
+    // Print ticker and percent change.
+    lcd.print(displayData[0]);
+
+    // Set cursor to second line.
+    lcd.setCursor(0, 1);
+
+    // Print last price.
+    lcd.print(displayData[1]);
+
+  }
+
+  // Send a 1 to computer (requests next ticker).
+  Serial.write(1);
+
+  // Wait 5 seconds.
+  delay(5000);
+}
